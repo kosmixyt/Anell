74e74a9e7bbed5f5d7aba422a1f8e6ba7989f441
kosmixyt <67480533+kosmixyt@users.noreply.github.com>
2022-05-21 19:08:18 +0200

    Anell Main
    First push

---
+++ assets/js/kl.js
@@ -0,0 +1,60 @@
+const makeid = length => {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
+
+
+function _(element){
+return document.getElementById(element);
+}
+
+function request(url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+
+function isset(element)
+{
+
+try{
+
+
+if(typeof(element) == 'undefined'){
+
+return false;
+}else{
+return true;
+}
+
+}catch(e){
+return false;
+}
+
+
+}
